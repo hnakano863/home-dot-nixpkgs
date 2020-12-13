d91b263dcfe0c5079144e8f948debb7d7400ab80
hnakano863 <notchi863@gmail.com>
2020-12-14 00:26:40 +0900

    更新

--- overlays/emacs-overlay/default.nix
+++ overlays/emacs-overlay/default.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchTarball {
   url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
-  sha256 = "1gkwa53z3j74mgpm79bk0739v2dzm3a2xhf4sw0c04sr6fn924rq";
+  sha256 = "0c3jlxc47dnx0l86y00h36mw9c1d8na37fz7xc6zl31f6f5fhz8c";
 })
