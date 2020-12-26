bfad6d88a8e3bab51d16ec675f3f11929f90d812
hnakano863 <notchi863@gmail.com>
2020-12-26 14:20:27 +0900

    Plots.jlからpyplotを使うための設定を追加

--- home/julia/default.nix
+++ home/julia/default.nix
@@ -2,9 +2,10 @@
 with pkgs;
 with lib;
 let
-  startup-jl = runCommand "startup.jl" { inherit jupyterCmdFHS; } ''
-    substituteAll "${./startup.jl.in}" $out
-  '';
+  pyCallEnv = python3.withPackages ( ps: with ps; [ matplotlib ]);
+  startup-jl = runCommand "startup.jl" {
+    inherit jupyterCmdFHS pyCallEnv;
+  } ''substituteAll "${./startup.jl.in}" $out '';
 in
 {
   home.packages = [
